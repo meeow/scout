fb871dded62564b370354f0eb2a3ba89831654a6
Brandon Hong <byhong@andrew.cmu.edu>
2019-10-31 21:33:35 -0700

    add more comments to discord bot, add empty py files

--- discord_bot.py
+++ discord_bot.py
@@ -7,30 +7,34 @@ token = base64.b64decode(token).decode('utf-8')
 
 bot = commands.Bot(command_prefix='!')
 
-# On Ready
+# Run this when the bot loads
 @bot.event
 async def on_ready():
-    status = 'Say !help' #'DOWN FOR MAINT' # Set the bot status msg
+    status = 'Say !help' # Set the bot status msg
     await bot.change_presence(activity=discord.Game(name=status))
     print('Logged in as: {}'.format(bot.user.name))
     print("Currently active on servers:\n{}".format('\n'.join([guild.name for guild in bot.guilds])))
     print('-' * 20)
 
-# Help
-bot.remove_command('help')
-@bot.command()
+# Help command
+bot.remove_command('help') # Remove default help command
+@bot.command() 
 async def help(ctx):
     title = "Help"
     description = "List of commands are:" 
+
+    # Make an embed object (stylized discord message)
     embed = discord.Embed(title=title, description=description, color=0xeee657)
 
     embed.add_field(
         name="!help", 
-        value='''Display commands.`''', 
+        value='''Display available commands.''', 
         inline=False)
 
+    # Send the help message
     await ctx.send(embed=embed)
 
+# Start bot
 bot.run(token)
 
 
