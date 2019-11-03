311fc8ccb4a0def4cd32093347756cf84105077d
Brandon Hong <byhong@andrew.cmu.edu>
2019-11-02 18:34:56 -0700

    sr command basic funtionality

--- discord_bot.py
+++ discord_bot.py
@@ -3,26 +3,32 @@ import discord
 import yaml
 from discord.ext import commands
 
+import get_player_stats
+
+# Temp token, implement better security later
 token = b'TmpNNU5qWTNOemM1TmpreE16UTNPVGN3LlhidW5GZy5LVWtPUzVqUjhXVTFBRGYzd1VjVl9CT3M4ckU='
 token = base64.b64decode(token).decode('utf-8')
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='$')
 
 # Run this when the bot loads
 @bot.event
 async def on_ready():
-    status = 'Say !help' # Set the bot status msg
+    status = 'Say $help' # Set the bot status msg
     await bot.change_presence(activity=discord.Game(name=status))
     print('Logged in as: {}'.format(bot.user.name))
     print("Currently active on servers:\n{}".format('\n'.join([guild.name for guild in bot.guilds])))
     print('-' * 20)
 
+@bot.command()
+async def sr(ctx, btag):
+    stats = get_player_stats.get_summary_stats(btag)
+    await ctx.send(stats)
+
 # Help command
 bot.remove_command('help') # Remove default help command
 @bot.command() 
 async def help(ctx):
-    title = "Help"
-    description = "List of commands are:" 
     commands = {}
 
     # Load help file
@@ -30,7 +36,7 @@ async def help(ctx):
         commands = yaml.safe_load(f)
 
     # Make an embed object (stylized discord message)
-    embed = discord.Embed(title=title, description=description, color=0xeee657)
+    embed = discord.Embed(title="Help", color=0xeee657)
 
     for command in commands:
         command_name = list(command.keys())[0]
--- get_player_stats.py
+++ get_player_stats.py
@@ -3,35 +3,47 @@ import json
 
 # @param btag: battletag in format Meeow#1317 or Meeow-1317
 # @return: dict of json response from api
-def get_stats(btag: str) -> dict:
+def get_full_stats(btag: str) -> dict:
     def format_btag(btag: str) -> str:
         btag = btag.replace('#', '-')
         return btag
     
-    # TODO @super
+    # TODO @super - try to use regex
     # @param btag: string that is checked for proper btag format
     # @return: false if not btag, true if possibly a valid btag
     def validate_btag(btag: str) -> bool:
         return True
 
-    api_url_prototype = 'https://ovrstat.com/stats/pc/{}'
-
     btag = format_btag(btag)
-
     # Invalid btag
     if not validate_btag(btag):
-        return {"error": "invalid btag"}
+        raise ValueError
     
+    api_url_prototype = 'https://ovrstat.com/stats/pc/{}'
     api_url = api_url_prototype.format(btag)
+
     response = requests.get(api_url).text
     result = json.loads(response)
 
     return result
 
+# @param btag: battletag in format Meeow#1317 or Meeow-1317
+# @return: SR and top 3 most played heroes
+def get_summary_stats(btag: str) -> dict:
+    full_stats = get_full_stats(btag)
+    if full_stats["private"]:
+        return {"error": "private profile"}
+    else:
+        summary_stats = {}
+        ratings = full_stats['ratings']
+        for rating in ratings:
+            summary_stats[rating['role']] = rating['level']
+        return summary_stats
+
 # manual testing
 if __name__ == "__main__":
     from pprint import pprint as print
     
-    btag = "Meeow#1317"
-    test = get_stats(btag)
+    btag = "Mystx#1209" #HachiYuki#4141"
+    test = get_summary_stats(btag)
     print(test)
